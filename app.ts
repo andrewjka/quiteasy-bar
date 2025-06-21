50f1b9beef26515ed7fd81afe73d75c49ec4f9d5
andrewjka <halliscode.js@gmail.com>
2025-06-21 11:12:17 +0300

    Renaming

--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
-import { App } from "astal/gtk3"
+import {App} from "astal/gtk3"
 import style from "./style.scss"
 import SlightUIBAR from "./widgets/Bar"
 
 App.start({
     css: style,
+    instanceName: 'Quiteasy-bar',
     main() {
         App.get_monitors().map(SlightUIBAR)
     },
